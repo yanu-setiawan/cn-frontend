0f344bf4d1002bfbd6bfe7ddb534cd59a93d5763
yanu-setiawan <yanusetiawan777@gmail.com>
2026-08-20 23:44:23 +0700

    chore: setup tailwindcss v4, heroui, and brand color

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { HeroUIProvider } from "@heroui/react";
+import App from "./App.tsx";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
+import Loader from "./components/Loader/index.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HeroUIProvider locale="en-GB">
+      <Suspense fallback={<Loader />}>
+        <App />
+        <Toaster position="top-center" />
+      </Suspense>
+    </HeroUIProvider>
+  </React.StrictMode>,
+);
