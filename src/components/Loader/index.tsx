f306f59a6ba4217828e615eff1c6a206c092cddc
yanu-setiawan <yanusetiawan777@gmail.com>
2026-08-20 23:45:27 +0700

    feat: setup and configure pages

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,7 @@
+export default function LoaderPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white/50 z-99">
+      <div className="loader"></div>
+    </div>
+  );
+}
---
+++ src/pages/WelcomePage/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/react";
+
+const WelcomePage = () => {
+  return (
+    <section>
+      <div className="text-2xl font-bold text-accent-primary">WelcomePage</div>
+      <Button color="primary">Hallo</Button>
+    </section>
+  );
+};
+
+export default WelcomePage;
