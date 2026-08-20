e09ffabb183c6cd26205332007f0eec7e23daf40
yanu-setiawan <yanusetiawan777@gmail.com>
2026-08-20 23:16:00 +0700

    chore: install core dependencies and setup prettier

---
+++ prettier.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: "lf",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+};
+
+export default config;
